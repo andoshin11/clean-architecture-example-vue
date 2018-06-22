75b063f15764c2c94f1b92f1d04f52145085876f
andoshin11 <shinglish11@gmail.com>
2018-06-22 16:14:07 +0900

    Migrate to Pages

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
-import About from "./views/About.vue";
+import Home from "./pages/Home.vue";
+import About from "./pages/About.vue";
 
 Vue.use(Router);
 
