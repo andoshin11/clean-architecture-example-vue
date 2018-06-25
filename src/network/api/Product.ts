6ff2a902b95ebe9c8b9c2e5301cb56b448c05f85
andoshin11 <shinglish11@gmail.com>
2018-06-25 23:53:54 +0900

    Add parser on Product API

--- src/network/api/Product.ts
+++ src/network/api/Product.ts
@@ -1,3 +1,4 @@
+import { AxiosResponse } from 'axios'
 import { APIRequest } from '@/network/APIRequest';
 import { HTTPMethod } from '@/network/APIClient';
 import { IProduct, IProductsCriteria } from '@/entities/Product'
@@ -8,6 +9,7 @@ export namespace ProductAPI {
     response: List<IProduct>;
     path = '/api/products';
     method = HTTPMethod.GET;
+    parse = (data: AxiosResponse) => data.data
     constructor(public params: PaginationRequest<IProductsCriteria>) {}
   }
 }
