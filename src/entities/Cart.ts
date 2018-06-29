05d32d3839ba4db97bddc9d568726bbe02970a47
andoshin11 <shinglish11@gmail.com>
2018-06-30 00:49:57 +0900

    Add Cart entity

---
+++ src/entities/Cart.ts
@@ -0,0 +1,25 @@
+import { IProduct } from '@/entities/Product'
+import store from '@/store'
+import { AddItem } from '@/store/modules/cart/types'
+
+export interface ICartItem extends IProduct {
+  checkoutPrice: number;
+}
+
+export interface ICart {
+  items: ICartItem[]
+}
+
+export class CartEntity {
+  constructor() {}
+
+  addItemToCart(item: IProduct) {
+    // do some price calculation.
+    const checkoutPrice = item.retailPrice * 1.08
+    const cartItem = {
+      ...item,
+      checkoutPrice
+    }
+    store.commit(new AddItem(cartItem))
+  }
+}
